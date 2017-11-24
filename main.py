dd6bc68d06932b72513e9d9a11469568214f2e7a
Rafal Gliniak <gliniakr@gmail.com>
2017-11-24 15:41:04 +0100

    chart data prepared

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 
 
 db = sqlite3.connect('example.db', check_same_thread=False)
-db.execute('DROP TABLE temperatures')
-db.execute('CREATE TABLE temperatures (id TEXT, value TEXT, time TEXT)')
-db.execute('insert into temperatures values ("4", "5", "03.12")')
-db.commit()
+# db.execute('DROP TABLE temperatures')
+# db.execute('CREATE TABLE temperatures (id TEXT, value TEXT, time TEXT)')
+# db.execute('insert into temperatures values ("4", "5", "03.12")')
+# db.commit()
 
 start_time = datetime.datetime.now().timestamp()
 
@@ -43,16 +43,23 @@ def prepare_chart():
     names = list(db.execute('SELECT DISTINCT(id) FROM temperatures'))
     names = [name[0] for name in names]
     print(names)
-    result = []
+    result = {}
     for name in names:
-        result.extend(list(db.execute(
+        result[name] = (list(db.execute(
             'SELECT * '
             'FROM temperatures '
             'WHERE id = ? '
             'ORDER BY time '
-            'DESC LIMIT 3', [name])))
-    result = [str(record) for record in result]
-    return render_template('template.html', name='\n'.join(result))
+            'DESC LIMIT 10', [name])))
+    # print(result)
+
+    for name, values_list in result.items():
+        result[name] = [(tuple_elem[2], int(tuple_elem[1])) for tuple_elem in values_list]
+
+    # print(result)
+
+    # result = [str(record) for record in result]
+    return render_template('template.html', name=result)
 
 
 def send_data(request_data):

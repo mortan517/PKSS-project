81f5c2184fd68c0adda092ff4864ef4b22f321c0
Rafal Gliniak <gliniakr@gmail.com>
2017-12-01 14:30:35 +0100

    chart data prepared, first chart tests

--- main.py
+++ main.py
@@ -50,15 +50,15 @@ def prepare_chart():
             'FROM temperatures '
             'WHERE id = ? '
             'ORDER BY time '
-            'DESC LIMIT 10', [name])))
+            'DESC LIMIT 50', [name])))
     # print(result)
 
     for name, values_list in result.items():
-        result[name] = [(tuple_elem[2], int(tuple_elem[1])) for tuple_elem in values_list]
+        result[name] = [str([tuple_el[2] for tuple_el in values_list][::-1]),
+                        [str(tuple_el[1]) for tuple_el in values_list][::-1]]
 
     # print(result)
 
-    # result = [str(record) for record in result]
     return render_template('template.html', name=result)
 
 
@@ -72,7 +72,7 @@ def send_data(request_data):
 
 @app.route('/time', methods=['GET'])
 def time():
-    speedup = 10
+    speedup = 100
     return str(speedup * (datetime.datetime.now().timestamp() - start_time))
 
 

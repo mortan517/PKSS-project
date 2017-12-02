47a9698f9d4549c2c22521e428ccf04457862152
daniellozuz <daniel.zuziak@gmail.com>
2017-12-02 18:59:44 +0100

    Refactoring.

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from flask import Flask, request, render_template
 import sqlite3
 import datetime
 
+now = datetime.datetime.now
+strptime = datetime.datetime.strptime
+
 app = Flask(__name__)
 
 db = sqlite3.connect('example.db', check_same_thread=False)
@@ -25,8 +28,7 @@ def flask_server():
 
 
 def get_particular_data(request_args):
-    names = list(request_args.getlist(key)
-                 for key in request_args.keys())[0]
+    names = list(request_args.getlist(key) for key in request_args.keys())[0]
     result = []
     for name in names:
         result.append(list(db.execute(
@@ -41,7 +43,6 @@ def get_particular_data(request_args):
 def prepare_chart():
     names = list(db.execute('SELECT DISTINCT(id) FROM temperatures'))
     names = [name[0] for name in names]
-    # print(names)
     result = {}
     for name in names:
         result[name] = (list(db.execute(
@@ -50,24 +51,18 @@ def prepare_chart():
             'WHERE id = ? '
             'ORDER BY time '
             'DESC LIMIT 50', [name])))
-    # print(result)
-
     for name, values_list in result.items():
-        result[name] = [str([int((datetime.datetime.now() - datetime.datetime.strptime(tuple_el[2],'%Y-%m-%d %H:%M:%S.%f')).total_seconds()) for tuple_el in values_list][::-1]),
-                        [str(tuple_el[1]) for tuple_el in values_list][::-1]]
-
-    print(result)
-    # TODO Change time to seconds so that time characteristics are visible on the plot.
-    # dt = datetime.strptime(xx, '%Y-%m-%d %H:%M:%S.%f')
-    # datetime.timedelta(hours=x.tm_hour,minutes=x.tm_min,seconds=x.tm_sec).total_seconds()
-
+        times = str([int((now() - strptime(tuple_el[2], '%Y-%m-%d %H:%M:%S.%f')).total_seconds())
+                     for tuple_el in values_list][::-1])
+        values = [str(tuple_el[1]) for tuple_el in values_list][::-1]
+        result[name] = [times, values]
     return render_template('web.html', name=result)
 
 
 def send_data(request_data):
     for name, value in request_data.form.to_dict().items():
         db.execute('INSERT INTO temperatures VALUES (?, ?, ?)',
-                   [name, value, datetime.datetime.now()])
+                   [name, value, now()])
         db.commit()
     return str(request_data.form.to_dict())
 
@@ -75,7 +70,7 @@ def send_data(request_data):
 @app.route('/time', methods=['GET'])
 def time():
     speedup = 100
-    return str(speedup * (datetime.datetime.now().timestamp() - start_time))
+    return str(speedup * (now().timestamp() - start_time))
 
 
 app.run(host='0.0.0.0', port='80', threaded=True)
